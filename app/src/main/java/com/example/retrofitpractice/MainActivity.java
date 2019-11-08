85065261c4985390ff227a11847f43ee3599cab4
vasu <vsvllbh143@gmail.com>
2019-11-09 00:32:48 +0530

    Shared Preferences Second Commit

--- app/src/main/java/com/example/retrofitpractice/MainActivity.java
+++ app/src/main/java/com/example/retrofitpractice/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.retrofitpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.retrofitpractice.POJO.mtest;
@@ -14,6 +17,11 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     API_EndPoints api_endPoints;
+
+
+    //use shared preference to store and retrive the data
+    public static final String PREFS_GAME ="com.example.retrofitpractice.GamePlay";
+    public static final String GAME_SCORE= "GameScore";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,"Success",Toast.LENGTH_LONG).show();
                 mtest user1 = response.body();
                 Toast.makeText(getApplicationContext(), user1.name + " " + user1.job + " " + user1.id + " " + user1.createdAt, Toast.LENGTH_SHORT).show();
+                saveLoginDetails(user1.name ,user1.job);
 
             }
 
@@ -39,4 +48,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void saveLoginDetails(String name, String job) {
+        SharedPreferences sharedPreferences = getSharedPreferences("LoginDetails", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("Email", name);
+        editor.putString("Password", job);
+        editor.commit();
+
+        Log.d("myPreferences",""+getEmail());
+    }
+
+    private String getEmail() {
+        SharedPreferences sharedPreferences = getSharedPreferences("LoginDetails", Context.MODE_PRIVATE);
+        return sharedPreferences.getString("Email", "");
+    }
+
 }
